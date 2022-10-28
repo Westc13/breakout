4ff2e203573077795ae5c5b3e925ead1d399511e
ChristineWest <christinewwwest@gmail.com>
2022-10-28 19:42:03 -0400

    added logic to create multiple blocks by looping through a blocks array and using constructor to create blocks array

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 const grid = document.querySelector(".grid");
-const block = document.createElement("div");
-block.classList.add("block");
-grid.appendChild(block);
+const blockWidth = 100;
+const blockHeight = 20;
+
+// create block individual
+
+class Block {
+  constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis];
+    this.topLeft = [xAxis, yAxis + blockHeight];
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight];
+  }
+}
+
+// all the blocks
+
+const blocks = [new Block(10, 270)];
+
+// add block function
+function addBlock() {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.style.left = "100px";
+  block.style.bottom = "50px";
+  grid.appendChild(block);
+}
+
+addBlock();
