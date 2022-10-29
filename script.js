0b89b1bf2c441f10444954b989b2165749fea0f1
ChristineWest <christinewwwest@gmail.com>
2022-10-29 11:25:34 -0400

    set if conditions to prevent ball leaving top and right border

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
 const boardWidth = 560;
+const boardHeight = 300;
 const ballDiameter = 20;
 let timerId;
 let xDirection = 2;
@@ -105,8 +106,8 @@ grid.appendChild(ball);
 function moveBall() {
   ballCurrentPosition[0] += xDirection;
   ballCurrentPosition[1] += yDirection;
-    drawBall();
-    checkForCollisions()
+  drawBall();
+  checkForCollisions();
 }
 
 timerId = setInterval(moveBall, 30);
@@ -114,15 +115,29 @@ timerId = setInterval(moveBall, 30);
 // check for collisions
 function checkForCollisions() {
   // check for wall collisions
-  if (ballCurrentPosition[0] >= (boardWidth - ballDiameter) || ballCurrentPosition[1]>=(boardHeight - ballDiameter)){
+  if (
+    ballCurrentPosition[0] >= boardWidth - ballDiameter ||
+    ballCurrentPosition[1] >= boardHeight - ballDiameter
+  ) {
     changeDirection();
   }
 }
 
 function changeDirection() {
-    if (xDirection === 2 && yDirection === 2) {
-        xDirection = -2
-        return
-    }
-    if(){}
+  if (xDirection === 2 && yDirection === 2) {
+    yDirection = -2;
+    return;
+  }
+  if (xDirection === 2 && yDirection === -2) {
+    xDirection = -2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === -2) {
+    yDirection = 2;
+    return;
+  }
+  if (xDirection === -2 && yDirection === 2) {
+    xDirection = 2;
+    return;
+  }
 }
