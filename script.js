3cf57ec47c9d449eed99047965ac3342587f44eb
ChristineWest <christinewwwest@gmail.com>
2022-10-28 20:17:14 -0400

    added ball

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector(".grid");
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWidth = 560;
 
 const userStart = [230, 10];
 let currentPosition = userStart;
@@ -71,7 +72,17 @@ function moveUser(e) {
         drawUser();
       }
       break;
+    case "ArrowRight":
+      if (currentPosition[0] < boardWidth - blockWidth) {
+        currentPosition[0] += 10;
+        drawUser();
+      }
   }
 }
 
-document.activeElement("keydown", moveUser);
+document.addEventListener("keydown", moveUser);
+
+// add ball
+const ball = document.createElement("div");
+ball.classList.add("ball");
+grid.appendChild(ball);
